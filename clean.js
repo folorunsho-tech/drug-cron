92ad6cfec4a8edac8c4a01dcc07f8f1c70bc732b
Folorunsho Ibrahim <folorunshoibr@gmail.com>
2026-02-02 15:40:38 +0100

    made it into scripts

---
+++ clean.js
@@ -0,0 +1,15 @@
+const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+
+const cleanJob = async () => {
+	console.log("Clean Summary started at " + new Date());
+	try {
+		const res = await fetch(baseUrl + "/api/summarizer/tally_cleaner", {
+			method: "DELETE",
+		});
+		console.log("Response Status: " + res.status + " " + res.statusText);
+	} catch (error) {
+		console.error("Error fetching tally cleaner endpoint: " + error);
+	}
+	console.log("Clean Summary ended at " + new Date());
+};
+cleanJob();
---
+++ init.js
@@ -0,0 +1,14 @@
+const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+const initjob = async () => {
+	console.log("Init Summary started at " + new Date());
+	try {
+		const res = await fetch(baseUrl + "/api/summarizer/init", {
+			method: "GET",
+		});
+		console.log("Response Status: " + res.status + " " + res.statusText);
+	} catch (error) {
+		console.error("Error fetching init endpoint: " + error);
+	}
+	console.log("Init Summary ended at " + new Date());
+};
+initjob();
